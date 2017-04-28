d34d854ca5bd8e6683d388703fef482879a6be69
Tomislav Bradarić <tomo@shoutem.com>
2017-04-28 16:15:43 +0200

    Add basic support for converting ical event ATTACH.

--- ical.js
+++ ical.js
@@ -2,12 +2,12 @@ var UUID = require('node-uuid');
 
 (function(name, definition) {
 
-/****************
- *  A tolerant, minimal icalendar parser
- *  (http://tools.ietf.org/html/rfc5545)
- *
- *  <peterbraden@peterbraden.co.uk>
- * **************/
+  /****************
+   *  A tolerant, minimal icalendar parser
+   *  (http://tools.ietf.org/html/rfc5545)
+   *
+   *  <peterbraden@peterbraden.co.uk>
+   * **************/
 
   if (typeof module !== 'undefined') {
     module.exports = definition();
@@ -19,14 +19,14 @@ var UUID = require('node-uuid');
 
 }('ical', function(){
 
-   // Unescape Text re RFC 4.3.11
+  // Unescape Text re RFC 4.3.11
   var text = function(t){
     t = t || "";
     return (t
-      .replace(/\\\,/g, ',')
-      .replace(/\\\;/g, ';')
-      .replace(/\\[nN]/g, '\n')
-      .replace(/\\\\/g, '\\')
+        .replace(/\\\,/g, ',')
+        .replace(/\\\;/g, ';')
+        .replace(/\\[nN]/g, '\n')
+        .replace(/\\\\/g, '\\')
     )
   }
 
@@ -35,9 +35,9 @@ var UUID = require('node-uuid');
     for (var i = 0; i<p.length; i++){
       if (p[i].indexOf('=') > -1){
         var segs = p[i].split('=');
-        
+
         out[segs[0]] = parseValue(segs.slice(1).join('='));
-        
+
       }
     }
     return out || sp
@@ -46,7 +46,7 @@ var UUID = require('node-uuid');
   var parseValue = function(val){
     if ('TRUE' === val)
       return true;
-    
+
     if ('FALSE' === val)
       return false;
 
@@ -58,34 +58,34 @@ var UUID = require('node-uuid');
   }
 
   var storeValParam = function (name) {
-      return function (val, curr) {
-          var current = curr[name];
-          if (Array.isArray(current)) {
-              current.push(val);
-              return curr;
-          }
-
-          if (current != null) {
-              curr[name] = [current, val];
-              return curr;
-          }
-
-          curr[name] = val;
-          return curr
+    return function (val, curr) {
+      var current = curr[name];
+      if (Array.isArray(current)) {
+        current.push(val);
+        return curr;
       }
+
+      if (current != null) {
+        curr[name] = [current, val];
+        return curr;
+      }
+
+      curr[name] = val;
+      return curr
+    }
   }
 
   var storeParam = function (name) {
-      return function (val, params, curr) {
-          var data;
-          if (params && params.length && !(params.length == 1 && params[0] === 'CHARSET=utf-8')) {
-              data = { params: parseParams(params), val: text(val) }
-          }
-          else
-              data = text(val)
-
-          return storeValParam(name)(data, curr);
+    return function (val, params, curr) {
+      var data;
+      if (params && params.length && !(params.length == 1 && params[0] === 'CHARSET=utf-8')) {
+        data = { params: parseParams(params), val: text(val) }
       }
+      else
+        data = text(val)
+
+      return storeValParam(name)(data, curr);
+    }
   }
 
   var addTZ = function (dt, params) {
@@ -100,7 +100,7 @@ var UUID = require('node-uuid');
 
 
   var dateParam = function(name){
-      return function (val, params, curr) {
+    return function (val, params, curr) {
 
       var newDate = text(val);
 
@@ -149,12 +149,12 @@ var UUID = require('node-uuid');
         }
 
         newDate = addTZ(newDate, params);
-    }
-
-
-          // Store as string - worst case scenario
-      return storeValParam(name)(newDate, curr)
       }
+
+
+      // Store as string - worst case scenario
+      return storeValParam(name)(newDate, curr)
+    }
   }
 
 
@@ -174,30 +174,59 @@ var UUID = require('node-uuid');
       if (curr[name] === undefined)
         curr[name] = val ? val.split(separatorPattern) : []
       else
-        if (val)
-          curr[name] = curr[name].concat(val.split(separatorPattern))
+      if (val)
+        curr[name] = curr[name].concat(val.split(separatorPattern))
       return curr
     }
   }
+
+  var attachmentParam = function(name){
+    return function(val, params, curr){
+      var attachment = {
+        url: val,
+      };
+
+      for (var i = 0; i < params.length; i++) {
+        var p = params[i];
+
+        var key = p.split('=')[0];
+        var value = p.split('=')[1];
+
+        if (!(value == null || typeof value == 'undefined')) {
+          attachment[key] = value;
+        } else {
+          attachment[key] = true;
+        }
+      }
+
+      if (curr[name] == null || typeof curr[name] == 'undefined') {
+        curr[name] = [];
+      }
+
+      curr[name].push(attachment);
+
+      return curr;
+    }
+  };
 
   // EXDATE is an entry that represents exceptions to a recurrence rule (ex: "repeat every day except on 7/4").
   // There can be more than one of these in a calendar record, so we create an array of them.
   // The index into the array is the ISO string of the date itself, for ease of use.
   // i.e. You can check if ((curr.exdate != undefined) && (curr.exdate[date iso string] != undefined)) to see if a date is an exception.
   var exdateParam = function (name) {
-      return function (val, params, curr) {
-          var exdate = new Array();
-          dateParam(name)(val, params, exdate);
-          curr[name] = curr[name] || [];
-          curr[name][exdate[name].toISOString()] = exdate[name];
-          return curr;
-      }
+    return function (val, params, curr) {
+      var exdate = new Array();
+      dateParam(name)(val, params, exdate);
+      curr[name] = curr[name] || [];
+      curr[name][exdate[name].toISOString()] = exdate[name];
+      return curr;
+    }
   }
 
   // RECURRENCE-ID is the ID of a specific recurrence within a recurrence rule.
   // TODO:  It's also possible for it to have a range, like "THISANDPRIOR", "THISANDFUTURE".  This isn't currently handled.
   var recurrenceParam = function (name) {
-      return dateParam(name);
+    return dateParam(name);
   }
 
   var addFBType = function (fb, params) {
@@ -233,104 +262,104 @@ var UUID = require('node-uuid');
 
     objectHandlers : {
       'BEGIN' : function(component, params, curr, stack){
-          stack.push(curr)
+        stack.push(curr)
 
-          return {type:component, params:params}
-        }
+        return {type:component, params:params}
+      }
 
       , 'END' : function(component, params, curr, stack){
         // prevents the need to search the root of the tree for the VCALENDAR object
         if (component === "VCALENDAR") {
-            //scan all high level object in curr and drop all strings
-            var key,
-                obj;
-            
-            for (key in curr) {
-                if(curr.hasOwnProperty(key)) {
-                   obj = curr[key];
-                   if (typeof obj === 'string') {
-                       delete curr[key];
-                   }
-                }
+          //scan all high level object in curr and drop all strings
+          var key,
+            obj;
+
+          for (key in curr) {
+            if(curr.hasOwnProperty(key)) {
+              obj = curr[key];
+              if (typeof obj === 'string') {
+                delete curr[key];
+              }
             }
-            
-            return curr
+          }
+
+          return curr
         }
-        
+
         var par = stack.pop()
         if (curr.uid)
         {
-        	// If this is the first time we run into this UID, just save it.
-        	if (par[curr.uid] === undefined)
+          // If this is the first time we run into this UID, just save it.
+          if (par[curr.uid] === undefined)
+          {
+            par[curr.uid] = curr;
+          }
+          else
+          {
+            // If we have multiple ical entries with the same UID, it's either going to be a
+            // modification to a recurrence (RECURRENCE-ID), and/or a significant modification
+            // to the entry (SEQUENCE).
+
+            // TODO: Look into proper sequence logic.
+
+            if (curr.recurrenceid === undefined)
             {
-            	par[curr.uid] = curr;
+              // If we have the same UID as an existing record, and it *isn't* a specific recurrence ID,
+              // not quite sure what the correct behaviour should be.  For now, just take the new information
+              // and merge it with the old record by overwriting only the fields that appear in the new record.
+              var key;
+              for (key in curr) {
+                par[curr.uid][key] = curr[key];
+              }
+
             }
-            else
-            {
-                // If we have multiple ical entries with the same UID, it's either going to be a
-                // modification to a recurrence (RECURRENCE-ID), and/or a significant modification
-                // to the entry (SEQUENCE).
+          }
 
-                // TODO: Look into proper sequence logic.
+          // If we have recurrence-id entries, list them as an array of recurrences keyed off of recurrence-id.
+          // To use - as you're running through the dates of an rrule, you can try looking it up in the recurrences
+          // array.  If it exists, then use the data from the calendar object in the recurrence instead of the parent
+          // for that day.
 
-                if (curr.recurrenceid === undefined)
-                {
-                    // If we have the same UID as an existing record, and it *isn't* a specific recurrence ID,
-                    // not quite sure what the correct behaviour should be.  For now, just take the new information
-                    // and merge it with the old record by overwriting only the fields that appear in the new record.
-                    var key;
-                    for (key in curr) {
-                    	par[curr.uid][key] = curr[key];
-                    }
+          // NOTE:  Sometimes the RECURRENCE-ID record will show up *before* the record with the RRULE entry.  In that
+          // case, what happens is that the RECURRENCE-ID record ends up becoming both the parent record and an entry
+          // in the recurrences array, and then when we process the RRULE entry later it overwrites the appropriate
+          // fields in the parent record.
 
-                }
-            }
+          if (curr.recurrenceid != null)
+          {
 
-        	// If we have recurrence-id entries, list them as an array of recurrences keyed off of recurrence-id.
-        	// To use - as you're running through the dates of an rrule, you can try looking it up in the recurrences
-        	// array.  If it exists, then use the data from the calendar object in the recurrence instead of the parent
-        	// for that day.
+            // TODO:  Is there ever a case where we have to worry about overwriting an existing entry here?
 
-        	// NOTE:  Sometimes the RECURRENCE-ID record will show up *before* the record with the RRULE entry.  In that
-        	// case, what happens is that the RECURRENCE-ID record ends up becoming both the parent record and an entry
-        	// in the recurrences array, and then when we process the RRULE entry later it overwrites the appropriate
-			// fields in the parent record.
-
-        	if (curr.recurrenceid != null)
-        	{
-
-        		// TODO:  Is there ever a case where we have to worry about overwriting an existing entry here?
-
-        		// Create a copy of the current object to save in our recurrences array.  (We *could* just do par = curr,
-        		// except for the case that we get the RECURRENCE-ID record before the RRULE record.  In that case, we 
-        		// would end up with a shared reference that would cause us to overwrite *both* records at the point
-				// that we try and fix up the parent record.)
-        		var recurrenceObj = new Object();
-        		var key;
-        		for (key in curr) {
-        			recurrenceObj[key] = curr[key];
-        		}
-
-        		if (recurrenceObj.recurrences != undefined) {
-        			delete recurrenceObj.recurrences;
-        		}
-
-
-				// If we don't have an array to store recurrences in yet, create it.
-        		if (par[curr.uid].recurrences === undefined) {
-        			par[curr.uid].recurrences = new Array();
-            	}
-
-				// Save off our cloned recurrence object into the array, keyed by date.
-        		par[curr.uid].recurrences[curr.recurrenceid.toISOString()] = recurrenceObj;
+            // Create a copy of the current object to save in our recurrences array.  (We *could* just do par = curr,
+            // except for the case that we get the RECURRENCE-ID record before the RRULE record.  In that case, we
+            // would end up with a shared reference that would cause us to overwrite *both* records at the point
+            // that we try and fix up the parent record.)
+            var recurrenceObj = new Object();
+            var key;
+            for (key in curr) {
+              recurrenceObj[key] = curr[key];
             }
 
-        	// One more specific fix - in the case that an RRULE entry shows up after a RECURRENCE-ID entry,
-        	// let's make sure to clear the recurrenceid off the parent field.
-        	if ((par[curr.uid].rrule != undefined) && (par[curr.uid].recurrenceid != undefined))
-            {
-        		delete par[curr.uid].recurrenceid;
+            if (recurrenceObj.recurrences != undefined) {
+              delete recurrenceObj.recurrences;
             }
+
+
+            // If we don't have an array to store recurrences in yet, create it.
+            if (par[curr.uid].recurrences === undefined) {
+              par[curr.uid].recurrences = new Array();
+            }
+
+            // Save off our cloned recurrence object into the array, keyed by date.
+            par[curr.uid].recurrences[curr.recurrenceid.toISOString()] = recurrenceObj;
+          }
+
+          // One more specific fix - in the case that an RRULE entry shows up after a RECURRENCE-ID entry,
+          // let's make sure to clear the recurrenceid off the parent field.
+          if ((par[curr.uid].rrule != undefined) && (par[curr.uid].recurrenceid != undefined))
+          {
+            delete par[curr.uid].recurrenceid;
+          }
 
         }
         else
@@ -358,7 +387,7 @@ var UUID = require('node-uuid');
       , 'CREATED': dateParam('created')
       , 'LAST-MODIFIED': dateParam('lastmodified')
       , 'RECURRENCE-ID': recurrenceParam('recurrenceid')
-
+      , 'ATTACH': attachmentParam('attach')
     },
 
 
@@ -370,11 +399,11 @@ var UUID = require('node-uuid');
 
       //handling custom properties
       if(name.match(/X\-[\w\-]+/) && stack.length > 0) {
-          //trimming the leading and perform storeParam
-          name = name.substring(2);
-          return (storeParam(name))(val, params, ctx, stack, line);
+        //trimming the leading and perform storeParam
+        name = name.substring(2);
+        return (storeParam(name))(val, params, ctx, stack, line);
       }
-      
+
       return storeParam(name.toLowerCase())(val, params, ctx);
     },
 
@@ -408,11 +437,11 @@ var UUID = require('node-uuid');
         ctx = self.handleObject(name, value, params, ctx, stack, l) || {}
       }
 
-       // type and params are added to the list of items, get rid of them.
-       delete ctx.type
-       delete ctx.params
+      // type and params are added to the list of items, get rid of them.
+      delete ctx.type
+      delete ctx.params
 
-       return ctx
+      return ctx
     }
 
   }
